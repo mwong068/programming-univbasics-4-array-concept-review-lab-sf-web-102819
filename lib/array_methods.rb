e51a00767dd48d61146988582dd589769bcbf373
Megan Wong <mwong068@gmail.com>
2019-10-17 22:48:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    if 
+    if array[index] == value_to_find
+      return index
+    end
 end
 
 def find_max_value(array)
