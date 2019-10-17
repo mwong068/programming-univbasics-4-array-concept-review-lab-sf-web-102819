96da4ad844862ce7e9c53507399a8e3b8e7c3f55
Megan Wong <mwong068@gmail.com>
2019-10-17 22:49:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    if array[index] == value_to_find
-      return index
-    else
+    if array[index] !== value_to_find
       return nil
+    else
+      return index
     end
   end
 end
